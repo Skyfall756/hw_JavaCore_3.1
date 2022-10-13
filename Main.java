e4bb12d440c00bc9a58cf4afa4e72370cb779e6b
Vladimir Zotov <zva95@mail.ru>
2022-10-13 20:44:32 +0400

    add log to zipFiles

--- Main.java
+++ Main.java
@@ -53,13 +53,12 @@ public class Main {
         List<GameProgress> gp = Arrays.asList(
                 new GameProgress(100, 56, 12, 30),
                 new GameProgress(79, 115, 24, 10),
-                new GameProgress(50,20,35,80));
-
+                new GameProgress(50, 20, 35, 80));
 
 
         saveGame(savegames.getPath(), gp, sb);
 
-        zipFiles(savegames.getAbsolutePath(), savesPaths(savegames));
+        zipFiles(savegames.getAbsolutePath(), savesPaths(savegames), sb);
 
         deleteSaves(savegames, sb);
 
@@ -78,7 +77,7 @@ public class Main {
     }
 
 
-    public static void makeDir (File file, StringBuilder sb) {
+    public static void makeDir(File file, StringBuilder sb) {
         if (file.mkdir()) {
             sb.append(file.getName() + " создан\n");
         } else {
@@ -87,7 +86,7 @@ public class Main {
 
     }
 
-    public static void makeFile (File file, StringBuilder sb) {
+    public static void makeFile(File file, StringBuilder sb) {
         try {
             if (file.createNewFile()) {
                 sb.append(file.getName() + " создан\n");
@@ -97,17 +96,20 @@ public class Main {
         }
     }
 
-    public static void zipFiles(String path, List<String> saves) {
+    public static void zipFiles(String path, List<String> saves, StringBuilder sb) {
         try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(path + "/zip.zip"))) {
             for (String str : saves) {
+                String name = new File(str).getName();
                 try (FileInputStream fis = new FileInputStream(str)) {
-                    ZipEntry entry = new ZipEntry(new File(str).getName());
+                    ZipEntry entry = new ZipEntry(name);
                     zout.putNextEntry(entry);
                     byte[] buffer = new byte[fis.available()];
                     fis.read(buffer);
                     zout.write(buffer);
                     zout.closeEntry();
+                    sb.append(name + " заархивирован\n");
                 } catch (IOException ex) {
+                    sb.append(name + " ошибка архивирования");
                     System.out.println(ex.getMessage());
                 }
             }
@@ -117,9 +119,9 @@ public class Main {
 
     }
 
-    public static List <String> savesPaths(File saves) {
+    public static List<String> savesPaths(File saves) {
         List<String> paths = new ArrayList<>();
-        File[] file = saves.listFiles( new MyFileNameFilter(".dat"));
+        File[] file = saves.listFiles(new MyFileNameFilter(".dat"));
         for (int i = 0; i < file.length; i++) {
             String path = file[i].getAbsolutePath();
             paths.add(path);
@@ -127,11 +129,12 @@ public class Main {
         return paths;
     }
 
-    public static void deleteSaves (File savegame, StringBuilder sb) {
+    public static void deleteSaves(File savegame, StringBuilder sb) {
         for (File file : savegame.listFiles(new MyFileNameFilter(".dat"))) {
-            if(file.delete()) sb.append(file.getName() + " удален\n");
+            if (file.delete()) sb.append(file.getName() + " удален\n");
         }
     }
+
     public static void saveGame(String path, List<GameProgress> gp, StringBuilder sb) {
         for (int p = 0; p < gp.size(); p++) {
             String paths = path + "/save" + p + ".dat";
@@ -140,6 +143,7 @@ public class Main {
                 oos.writeObject(gp.get(p));
                 sb.append(gp.get(p) + " сохранен\n");
             } catch (IOException ex) {
+                sb.append(gp.get(p) + " ошибка сохранения\n");
                 System.out.println(ex.getMessage());
             }
         }
